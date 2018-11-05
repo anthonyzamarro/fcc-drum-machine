1b7f9585a5e58d33569f6ecd78a6436a89e81746
Anthony <anthony.zamarro@gmail.com>
2018-11-05 12:34:12 -0500

    Add Container
    add ids and container classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="App drum-machine-container" id="drum-machine">
+        <div className="display-container" id="display">
+          Drum Machine
+        </div>
       </div>
     );
   }
