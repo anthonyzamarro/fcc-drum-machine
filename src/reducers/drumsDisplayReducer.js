f8d8f924ab3c5ef564cff61319b0e724535dda02
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-11-07 22:28:57 -0500

    add other audio files

--- src/reducers/drumsDisplayReducer.js
+++ src/reducers/drumsDisplayReducer.js
@@ -1,4 +1,15 @@
+// “Sound effects obtained from https://www.zapsplat.com“
+// https://www.musicradar.com/news/drums/sampleradar-1000-free-drum-samples-229460
+// http://www.findsounds.com/ISAPI/search.dll
 import snare from '../audio/snare.mp3';
+import crash from '../audio/crash.mp3';
+import bass from '../audio/bass.wav';
+import floorTom from '../audio/floor-tom.wav';
+import tomTom from '../audio/tom-tom.wav';
+import ride from '../audio/ride.wav';
+import hiHat from '../audio/hi-hat.wav';
+import hiHatClosed from '../audio/hi-hat-closed.mp3';
+import gong from '../audio/gong.wav';
 
 export default function drumData() {
   return [
@@ -11,50 +22,50 @@ export default function drumData() {
     {
       id: 2,
       key: 'W',
-      type: '',
-      audio: ''
+      type: 'crash',
+      audio: crash
     },
     {
       id: 3,
       key: 'E',
-      type: '',
-      audio: ''
+      type: 'bass',
+      audio: bass
     },
     {
       id: 4,
       key: 'A',
-      type: '',
-      audio: ''
+      type: 'floorTom',
+      audio: floorTom
     },
     {
       id: 5,
       key: 'S',
-      type: '',
-      audio: ''
+      type: 'tomTom',
+      audio: tomTom
     },
     {
       id: 6,
       key: 'D',
-      type: '',
-      audio: ''
+      type: 'ride',
+      audio: ride
     },
     {
       id: 7,
       key: 'Z',
-      type: '',
-      audio: ''
+      type: 'hiHat',
+      audio: hiHat
     },
     {
       id: 8,
       key: 'X',
-      type: '',
-      audio: ''
+      type: 'hiHat',
+      audio: hiHatClosed
     },
     {
       id: 9,
       key: 'C',
-      type: '',
-      audio: ''
+      type: 'gong',
+      audio: gong
     }
   ];
 }
