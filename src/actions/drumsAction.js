b9c24b111b33acddbfd2e975ae596474250fc17a
Anthony <anthony.zamarro@gmail.com>
2018-11-09 13:25:26 -0500

    drumsInfo
    add drumsInfo component and hook up to redux. update reducer to reflect changed data

--- src/actions/drumsAction.js
+++ src/actions/drumsAction.js
@@ -1,5 +1,5 @@
 export function drumClicked(clicked) {
-  console.log("drumsAction: action is ", clicked);
+  // console.log("drumsAction: action is ", clicked);
   return {
     type: "DRUM_CLICKED",
     payload: clicked
@@ -7,7 +7,7 @@ export function drumClicked(clicked) {
 }
 
 export function drumPressed(pressed) {
-  console.log("drumsAction: action is ", pressed);
+  // console.log("drumsAction: action is ", pressed);
   return {
     type: "DRUM_PRESSED",
     payload: pressed
--- src/reducers/drumsReducer.js
+++ src/reducers/drumsReducer.js
@@ -1,13 +1,15 @@
 export default function(state = null, action) {
   switch (action.type) {
     case "DRUM_CLICKED":
-      console.log("drumsReducer: drum was clicked", action);
-      return action.payload;
+      console.log("drumsReducer, DRUM_CLICKED: drum was clicked", action);
+      const clickedDrum = Object.assign({ c: 4, d: 5 }, action.payload);
+      return clickedDrum;
     case "DRUM_PRESSED":
-      console.log("drumsReducer: drum was pressed", action);
-      return action.payload;
+      console.log("drumsReducer, DRUM_PRESSED: drum was pressed", action);
+      const pressedDrum = Object.assign({ c: 4, d: 5 }, action.payload);
+      return pressedDrum;
     default:
-      console.log("drumsReducer: nah son");
+      return state;
   }
   return state;
 }
