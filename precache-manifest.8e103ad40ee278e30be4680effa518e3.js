85220b1d222173535129af99e07b5ac3f2e0f223
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-11-10 16:57:09 -0500

    Updates

---
+++ precache-manifest.8e103ad40ee278e30be4680effa518e3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c7d1310e4f33c313d67010ba9a9a82",
+    "url": "/fcc-drum-machine/static/media/floor-tom.01c7d131.wav"
+  },
+  {
+    "revision": "17b1ced682bfe391d514",
+    "url": "/fcc-drum-machine/static/css/main.02bf1bd9.chunk.css"
+  },
+  {
+    "revision": "63c5c257d2d9d3e194d8",
+    "url": "/fcc-drum-machine/static/js/1.63c5c257.chunk.js"
+  },
+  {
+    "revision": "6b38bc727055cad7116d",
+    "url": "/fcc-drum-machine/static/js/runtime~main.6b38bc72.js"
+  },
+  {
+    "revision": "29931dfa02c34603314cffebd3b2c1d3",
+    "url": "/fcc-drum-machine/static/media/snare.29931dfa.mp3"
+  },
+  {
+    "revision": "ab3cc766b2b54d232315fd9a4227fe4c",
+    "url": "/fcc-drum-machine/static/media/crash.ab3cc766.mp3"
+  },
+  {
+    "revision": "5e9cce802a4bbcf4e4f6f086d992d440",
+    "url": "/fcc-drum-machine/static/media/bass.5e9cce80.wav"
+  },
+  {
+    "revision": "17b1ced682bfe391d514",
+    "url": "/fcc-drum-machine/static/js/main.17b1ced6.chunk.js"
+  },
+  {
+    "revision": "b40f796f08c784379dca2937f4075f0f",
+    "url": "/fcc-drum-machine/static/media/tom-tom.b40f796f.wav"
+  },
+  {
+    "revision": "3a196551f807a4f49a8a9ea5cea8e6d2",
+    "url": "/fcc-drum-machine/static/media/ride.3a196551.wav"
+  },
+  {
+    "revision": "f84399ad9b1e5820289d578e71240b9c",
+    "url": "/fcc-drum-machine/static/media/hi-hat.f84399ad.wav"
+  },
+  {
+    "revision": "0d385804a1b4704bc6404709801991db",
+    "url": "/fcc-drum-machine/static/media/hi-hat-closed.0d385804.mp3"
+  },
+  {
+    "revision": "5010ef2a17ed7189dd0f0af62adddb44",
+    "url": "/fcc-drum-machine/static/media/gong.5010ef2a.wav"
+  },
+  {
+    "revision": "52a509d4c516319520fd667f3deab237",
+    "url": "/fcc-drum-machine/index.html"
+  }
+];
