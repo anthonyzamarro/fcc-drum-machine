18d1a51a14748b4cb7879b25111e9ad4750f638d
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-11-12 09:37:44 -0500

    listen for keyCode rather than key property to pass test

--- src/components/drums.js
+++ src/components/drums.js
@@ -7,16 +7,19 @@ class Drums extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drum: this.props.drumData.drumData,
       current: 'active'
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   componentDidMount() {
-    // document.addEventListener("keydown", e => this.handleKeyPress(e));
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.addEventListener("keydown", this.handleKeyPress); 
   }
   handleClick(e, d) {
+    e.target.children[0].currentTime = 0;
     e.target.children[0].play();
     e.persist()
     e.target.classList.add(this.state.current);
@@ -26,11 +29,11 @@ class Drums extends Component {
     this.props.itemClicked(d);
   }
   handleKeyPress(e) {
-    this.state.drum.forEach(d => {
-      if (e.key === d.keyCode) {
+    this.props.drumData.drumData.forEach(d => {
+      if (e.keyCode === d.keyCode) {
         let currentDrum = document.querySelector(`#key-${d.key}`);
-        // console.log(currentDrum.children[0]);
-        currentDrum.children[0].play();
+        currentDrum.children[0].currentTime = 0;
+        currentDrum.click()
         currentDrum.classList.add(this.state.current);
         setTimeout(() => {
           currentDrum.classList.remove(this.state.current);
@@ -45,11 +48,9 @@ class Drums extends Component {
         <li
           key={drum.id}
           onClick={e => this.handleClick(e, drum)}
-          onKeyPress={e => this.handleKeyPress(e)}
           id={`key-${drum.key}`}
           className={`drum-pad`}
           tabIndex="0"
-          ref={li => li && li.focus()}
         >
           {drum.key}
           <audio src={drum.audio} className="clip" id={drum.key} />
--- src/reducers/drumsDisplayReducer.js
+++ src/reducers/drumsDisplayReducer.js
@@ -11,70 +11,90 @@ import hiHat from "../audio/hi-hat.wav";
 import hiHatClosed from "../audio/hi-hat-closed.mp3";
 import gong from "../audio/gong.wav";
 
+// import bigFootHh from "../audio/old/hi-hat.wav";
+// 81, 87, 69, 65, 83, 68, 90, 88, 67
 export default function drumData() {
   return [
     {
       id: 1,
       key: "Q",
-      keyCode: "q",
+      // keyCode: "q",
+      keyCode: 81,
       type: "Snare",
       audio: snare
+      // audio: bigFootHh
     },
     {
       id: 2,
       key: "W",
-      keyCode: "w",
+      // keyCode: "w",
+      keyCode: 87,
       type: "Crash",
       audio: crash
+      // audio: bigFootHh
     },
     {
       id: 3,
       key: "E",
-      keyCode: "e",
+      // keyCode: "e",
+      keyCode: 69,
       type: "Kick drum",
       audio: bass
+      // audio: bigFootHh
     },
     {
       id: 4,
       key: "A",
-      keyCode: "a",
+      // keyCode: "a",
+      keyCode: 65,
       type: "Floor tom",
       audio: floorTom
+      // audio: bigFootHh
     },
     {
       id: 5,
       key: "S",
-      keyCode: "s",
+      // keyCode: "s",
+      keyCode: 83,
       type: "Tom-tom",
       audio: tomTom
+      // audio: bigFootHh
     },
     {
       id: 6,
       key: "D",
-      keyCode: "d",
+      // keyCode: "d",
+      keyCode: 68,
       type: "Ride",
       audio: ride
+      // audio: bigFootHh
     },
     {
       id: 7,
       key: "Z",
-      keyCode: "z",
+      // keyCode: "z",
+      keyCode: 90,
       type: "Hi-hat",
       audio: hiHat
+      // audio: bigFootHh
     },
     {
       id: 8,
       key: "X",
-      keyCode: "x",
+      // keyCode: "x",
+      keyCode: 88,
       type: "Closed hi-hat",
       audio: hiHatClosed
+      // audio: bigFootHh
     },
     {
       id: 9,
       key: "C",
-      keyCode: "c",
+      // keyCode: "c",
+      keyCode: 67,
       type: "Gong",
       audio: gong
+      // audio: bigFootHh
     }
   ];
 }
